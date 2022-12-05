cca3fbdd4a3f3ad82e14cfb242a705b94aa62958
ZUBOV ILLIA <zubovillia@gmail.com>
2022-12-05 16:31:15 +0300

    implement next js api

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,25 @@
 import Heading from '../components/Heading';
 import styles from '../styles/Home.module.scss';
 import Head from 'next/head';
+import Socials from '../components/Socials';
 
-const Home = () => {
+export const getStaticProps = async () => {
+  // const response = await fetch('http://localhost:3001/api/socials');
+  const response = await fetch(`${process.env.API_HOST}/socials`);
+  const data = await response.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { socials: data },
+  }
+};
+
+const Home = ({ socials }) => {
   return (
     <div className={styles.wrapper}>
       <Head>
@@ -10,6 +27,7 @@ const Home = () => {
       </Head>
 
       <Heading text="Next.js Application" />
+      <Socials socials={socials} />
     </div>
   )
 };
