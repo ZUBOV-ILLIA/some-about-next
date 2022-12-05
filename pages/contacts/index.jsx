195d38ce04ff87473d8ae32c477e6bc7cebb1c0e
ZUBOV ILLIA <zubovillia@gmail.com>
2022-12-05 13:17:41 +0300

    fetch from api default react way

--- pages/contacts/index.jsx
+++ pages/contacts/index.jsx
@@ -1,14 +1,39 @@
 import Heading from "../../components/Heading";
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
-const Contacts = () => (
-  <>
-    <Head>
-      <title>Contacts</title>
-    </Head>
+// export const getStaticProps = async () => {
 
-    <Heading text="Contacts List:" />
-  </>
-);
+// };
+
+const Contacts = () => {
+  const [contacts, setContacts] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+
+      setContacts(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Contacts</title>
+      </Head>
+
+      <Heading text="Contacts List:" />
+
+      <ul>
+        {contacts && contacts.map(({ id, name, email }) => (
+          <li key={id}><strong>{name}</strong> ({email})</li>
+        ))}
+      </ul>
+    </>
+  )
+};
 
 export default Contacts;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,17 +2,16 @@ import Heading from '../components/Heading';
 import styles from '../styles/Home.module.scss';
 import Head from 'next/head';
 
-const Home = () => (
-  <>
-    <Head>
-      <title>Home</title>
-    </Head>
-
+const Home = () => {
+  return (
     <div className={styles.wrapper}>
+      <Head>
+        <title>Home</title>
+      </Head>
+
       <Heading text="Next.js Application" />
     </div>
-  </>
-
-);
+  )
+};
 
 export default Home;
